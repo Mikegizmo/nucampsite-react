7b356b4cf3f7cefbb75aa3b5ce89e60449284235
Michael Panter <makinglife1231@gmail.com>
2022-10-22 21:55:09 -0400

    useState

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,6 +4,10 @@ export const selectAllCampsites = () => {
   return CAMPSITES;
 };
 
-export const selectRandomCampsite = () => {
-  return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
+// export const selectRandomCampsite = () => {
+//   return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
+// };
+
+export const selectCampsitesById = (id) => {
+  return CAMPSITES.find((campsite) => campsite.id === id);
 };
