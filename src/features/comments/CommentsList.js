9bbb2640208d7ccc7ff74301e83fafb3b5b621a8
Michael Panter <makinglife1231@gmail.com>
2022-11-25 09:31:49 -0500

    week 5 workshop

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,13 +1,34 @@
 import { useSelector } from "react-redux";
-import { Col } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import Comment from './Comment';
 import { selectCommentsByCampsiteId } from "./commentsSlice";
 import CommentForm from "./CommentForm";
+import Error from "../../components/Error";
+import Loading from "../../components/Loading";
 
 const CommentsList = ({ campsiteId }) => {
   const comments = useSelector(selectCommentsByCampsiteId(campsiteId));
 
+  const isLoading = useSelector((state) => state.comments.isLoading);
+  const errMsg = useSelector((state) => state.comments.errMsg);
+
   if (comments && comments.length > 0) {
+    if (isLoading) {
+      return (
+        <Row>
+          <Loading />
+        </Row>
+      ); 
+    }
+  
+    if (errMsg) {
+      return (
+        <Row>
+          <Error errMsg={errMsg} />
+        </Row>
+      )
+    }  
+    
     return (
       <Col md='5' className='m-1'>
         <h4>Comments</h4>
